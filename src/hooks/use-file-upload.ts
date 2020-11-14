862a835c5c54fb469b46731f32c9bd61bf71bc8b
rodnikovdi <rodnikovdi@vtblife.ru>
2020-11-14 18:14:54 +0300

    feat: file upload hook

---
+++ src/hooks/use-file-upload.ts
@@ -0,0 +1,91 @@
+import { useCallback } from 'react';
+import { v4 as uuid } from 'uuid';
+
+export interface UploadProps {
+    uploadService: FileUploadService;
+    onFileAdd?: (file: File, id: string) => void;
+    onFileUploaded?: (fileData: FileData, id: string) => void;
+    onFileReject?: (error: DropzoneFileError, index: number, arr: any) => void;
+}
+
+export interface FileData {
+    fileId: string;
+    status: FileStatus;
+    meta: FileMeta;
+    blob?: Blob;
+}
+
+export enum FileStatus {
+    LOADING,
+    UPLOADED,
+    ERROR,
+}
+
+interface FileMeta {
+    name?: string;
+    originalFileName: string;
+}
+
+interface FileUploadService {
+    upload: (file: File, id: string) => Promise<FileUploadResponse>;
+}
+
+interface FileUploadResponse {
+    id: string;
+}
+
+interface DropzoneFileError {
+    message: string;
+    code: string;
+}
+
+export interface DropzoneFileRejection {
+    file: File;
+    errors: DropzoneFileError[];
+}
+
+export function useFileUpload({ uploadService, onFileAdd, onFileUploaded, onFileReject }: UploadProps) {
+    const handleDropReject = useCallback(
+        (rejections: DropzoneFileRejection[]) => {
+            if (onFileReject) {
+                rejections.forEach(rejection => {
+                    rejection.errors.forEach(onFileReject);
+                });
+            }
+        },
+        [onFileReject],
+    );
+
+    const handleDrop = useCallback(
+        (files: File[]) => {
+            files.forEach(file => {
+                const generatedId = uuid();
+
+                onFileAdd?.(file, generatedId);
+
+                const fileData: FileData = {
+                    fileId: '',
+                    status: FileStatus.LOADING,
+                    meta: { originalFileName: file.name },
+                    blob: file,
+                };
+
+                uploadService
+                    .upload(file, generatedId)
+                    .then(response => {
+                        fileData.status = FileStatus.UPLOADED;
+                        fileData.fileId = response.id || generatedId;
+                    })
+                    .catch(() => {
+                        fileData.status = FileStatus.ERROR;
+                    })
+                    .finally(() => {
+                        onFileUploaded?.(fileData, generatedId);
+                    });
+            });
+        },
+        [onFileAdd, onFileUploaded, uploadService],
+    );
+
+    return { handleDrop, handleDropReject };
+}
