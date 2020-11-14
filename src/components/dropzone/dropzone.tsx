eb1562e5e9e3935ab325af0ffc25c7860ffb686e
rodnikovdi <rodnikovdi@vtblife.ru>
2020-11-14 18:14:26 +0300

    feat: dropzone component

---
+++ src/components/dropzone/dropzone.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useDropzone } from 'react-dropzone';
+import cn from 'classnames';
+import styles from './dropzone.module.css';
+
+interface DropZoneProps {
+    disabled?: boolean;
+    onDrop: (files: File[]) => void;
+}
+
+export const DropZone = ({ onDrop, disabled }: DropZoneProps) => {
+    console.log(disabled);
+    const { getInputProps, getRootProps } = useDropzone({ onDrop, disabled });
+
+    return (
+        <div {...getRootProps()} className={cn(styles.dropZone, { [styles.disabled]: disabled })}>
+            <input {...getInputProps()} />
+            Press or drop files here
+        </div>
+    );
+};
