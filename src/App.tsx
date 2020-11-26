be4da180c472ae6eb758632a523e38eb2385f34e
rodnikovdi <rodnikovdi@vtblife.ru>
2020-11-26 10:44:16 +0300

    feat: add github link

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import styles from './App.module.scss';
 const App = () => {
     return (
         <div className={styles.main}>
+            <div className={styles.ghLink}>
+                <span>Check this project on </span>
+                <a href="https://github.com/dmitryrodnikov/file-upload-concept">GitHub</a>
+            </div>
             <h1>Upload documents</h1>
             <Form />
         </div>
